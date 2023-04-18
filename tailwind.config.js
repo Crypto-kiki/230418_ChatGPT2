b8d6d3c98315c5a9f0e4754f228d82eb40ee195f
Crypto-kiki <ejo2123@naver.com>
2023-04-18 16:28:34 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        color1: "#58626C",
+        color2: "#DAC8BE",
+        color3: "#ECC1A1",
+        color4: "#F5F1EE",
+        mutsa: "#C44FEB",
+        main: "#C44FEB",
+      },
+    },
+  },
+  plugins: [],
+};
